e2b666d4ce990051c3fb5b33b213273337001e41
Rajat <rajatdhoot123@gmail.com>
2019-08-25 17:42:41 +0530

    Updates

---
+++ precache-manifest.cb74ddea44cfa2aa15f1f2fac24921f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40446c359a742a8ad4c08724d97198b5",
+    "url": "/practice/index.html"
+  },
+  {
+    "revision": "b3d13ae1b9ad6f284b36",
+    "url": "/practice/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "433e90020c1cc2995150",
+    "url": "/practice/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b3d13ae1b9ad6f284b36",
+    "url": "/practice/static/js/2.b7126152.chunk.js"
+  },
+  {
+    "revision": "433e90020c1cc2995150",
+    "url": "/practice/static/js/main.e9c45521.chunk.js"
+  },
+  {
+    "revision": "538871e82424b21a074c",
+    "url": "/practice/static/js/runtime~main.215a9eed.js"
+  }
+]);
